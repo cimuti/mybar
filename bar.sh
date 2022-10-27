871672441bf9721e3af781d93038467f99b78cac
cimuti <116425767+cimuti@users.noreply.github.com>
2022-10-28 00:17:52 +0700

    Create bar.sh

---
+++ bar.sh
@@ -0,0 +1,13 @@
+clear
+
+cowsay -f eyes Citra Mutiara | lolcat
+
+toilet -f slant  "      Dor to Dor" | lolcat
+
+echo FatMuk█■█■█ "                                                         ▃ ▄ ▅ ▆" | lolcat
+
+echo ▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▶●●●●●●●●●◀▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣ | lolcat
+
+date | lolcat
+
+echo ▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▶●●●●●●●●●◀▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣ | lolcat
